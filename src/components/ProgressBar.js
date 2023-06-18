b1f8956d13aff01c4a5cb1f24dd4538c5df772c4
Curtis Hill <chillmax@MacBook.lan>
2023-06-17 20:54:32 -0700

    progress bar and set goal function

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const ProgressBar = () => {
+  const [goal, setGoal] = useState('');
+
+  useEffect(() => {
+    const storedGoal = localStorage.getItem('storedGoal');
+    if (storedGoal) {
+      setGoal(storedGoal);
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    const inputGoal = event.target.value;
+    setGoal(inputGoal);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('storedGoal', goal);
+  }, [goal]);
+
   return (
-    <div className="progress-bar">
-      <div className="progress"></div>
-    </div>
+    <>
+      <div className="goal-input">
+        <input
+          type="number"
+          id="setGoal"
+          value={goal}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="progress-bar">
+        <div className="progress"></div>
+        <p>0 / {goal} GRAMS</p>
+      </div>
+    </>
   );
 };
 
