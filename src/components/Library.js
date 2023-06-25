97373dbace7bd4c490fe2b04a0aef22d57eb9dca
Curtis Hill <chillmax@MacBook.lan>
2023-06-24 23:19:45 -0700

    build tracker component

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,12 +1,134 @@
-import React from 'react';
-import SavedFoods from './SavedFoods';
-import NewFood from './NewFood';
+import React, { useState, useEffect } from 'react';
 
 const Library = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [foodName, setFoodName] = useState('');
+  const [gramsOfProtein, setGramsOfProtein] = useState('');
+  const [measurement, setMeasurement] = useState('');
+  const [foodItems, setFoodItems] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    // Retrieve stored food items from local storage
+    const storedItems = localStorage.getItem('foodItems');
+    if (storedItems) {
+      const parsedItems = JSON.parse(storedItems);
+      const sortedItems = parsedItems.sort((a, b) =>
+        a.foodName.localeCompare(b.foodName)
+      );
+      setFoodItems(sortedItems);
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!foodName || !measurement || !gramsOfProtein) {
+      setErrorMessage('Please fill in all fields');
+      return;
+    }
+
+    const newFoodItem = {
+      foodName,
+      measurement,
+      gramsOfProtein: parseFloat(gramsOfProtein),
+    };
+
+    // Store newFoodItem in local storage
+    const updatedItems = [...foodItems, newFoodItem];
+    const sortedItems = updatedItems.sort((a, b) =>
+      a.foodName.localeCompare(b.foodName)
+    );
+    localStorage.setItem('foodItems', JSON.stringify(sortedItems));
+
+    // Update the foodItems state and clear the form fields
+    setFoodItems(sortedItems);
+    setFoodName('');
+    setGramsOfProtein('');
+    setMeasurement('');
+    setErrorMessage('');
+  };
+
+  const handleFormToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleDelete = (index) => {
+    const updatedItems = foodItems.filter((_, i) => i !== index);
+    localStorage.setItem('foodItems', JSON.stringify(updatedItems));
+    setFoodItems(updatedItems);
+  };
+
   return (
     <div className="library">
-      <SavedFoods />
-      <NewFood />
+      <div className="saved-foods">
+        <div className="section-title">
+          <h2>SAVED FOODS</h2>
+        </div>
+        <ul>
+          {foodItems.map((item, index) => (
+            <li key={index}>
+              <div
+                className="row"
+                style={{
+                  backgroundColor:
+                    index % 2 === 1 ? 'inherit' : 'var(--colorPrimary)',
+                }}
+              >
+                <div className="cell cell-food-name">{item.foodName}</div>
+                <div className="cell cell-grams-of-protein">
+                  {item.gramsOfProtein}g
+                </div>
+                <div className="cell cell-measurement">{item.measurement}</div>
+                <div className="cell cell-delete">
+                  <button onClick={() => handleDelete(index)}>Delete</button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button
+        className="add-food-btn"
+        onClick={handleFormToggle}
+      >
+        {isOpen ? 'ADD FOOD －' : 'ADD FOOD ＋'}
+      </button>
+      {isOpen && (
+        <form
+          className="add-food-form"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="input-food-name"
+            type="text"
+            placeholder="Name"
+            value={foodName}
+            onChange={(e) => setFoodName(e.target.value)}
+          />
+          <input
+            className="input-grams-of-protein"
+            type="number"
+            placeholder="Grams"
+            value={gramsOfProtein}
+            onChange={(e) => setGramsOfProtein(e.target.value)}
+          />
+          <input
+            className="input-measurement"
+            type="text"
+            placeholder="Units"
+            value={measurement}
+            onChange={(e) => setMeasurement(e.target.value)}
+          />
+          <button
+            className="input-submit"
+            type="submit"
+          >
+            Submit
+          </button>
+          {errorMessage && <p>{errorMessage}</p>}
+        </form>
+      )}
     </div>
   );
 };
